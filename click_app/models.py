4fa645ea672f254f6641cb32c914d42c38afc9e3
almaz <almaz.dzhumabaev@iaau.edu.kg>
2014-09-10 18:10:27 +0400

    version_6

---
+++ click_app/models.py
@@ -0,0 +1,67 @@
+from datetime import time
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class DurationField(models.Field):
+    description = "makes field from time class to time database type "
+    __metaclass__=models.SubfieldBase
+
+    def __init__(self,help_text=("A comma-separated latitude longitude pair"),verbose_name='durationfield', *args,**kwargs):
+        self.name="DurationField",
+        self.through = None
+        self.help_text = help_text
+        self.blank = True
+        self.editable = True
+        self.creates_table = False
+        self.db_column = None
+        self.serialize = False
+        self.null = True
+        self.creation_counter = models.Field.creation_counter
+        models.Field.creation_counter += 1
+        super(DurationField, self).__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return 'time'
+
+    def to_python(self, value):
+        if value in ( None,''):
+            return DurationField()
+        else:
+            if isinstance(value, time):
+                return value
+            else:
+                args = [int(value.split(':')[0]),int(value.split(':')[1]), int(value.split(':')[2])]
+                if len(args) != 3 and value is not None:
+                    raise ValidationError("Invalid input for a GeoCoordinate instance")
+                return time(*args)
+
+    def get_prep_value(self, value):
+        return ':'.join([str(value.hour),str(value.minute),str(value.second)])
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20, blank=False, help_text="product's model and some details")
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    owner = models.ForeignKey(User)
+    na_prodaje = models.BooleanField(default=False)
+    time = DurationField()
+
+
+    def __unicode__(self):
+        return  u'%s %s' % (self.name, self.time)
+
+
+class Duration(object):
+    def __init__(self, hour, minute, second):
+        self.hour=hour
+        self.minute=minute
+        self.second=second
+
+    def __repr__(self):
+        return str(self.hour)+':'+str(self.minute)+':'+str(self.second)
+
+
+
+
---
+++ flip_v6/settings.py
@@ -0,0 +1,93 @@
+"""
+Django settings for flip_v6 project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$d8iv0d#tx(nf(nrc1&!=&%9uke3#y1ubh$9tq6*y)5!q8at_t'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'click_app',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'flip_v6.urls'
+
+WSGI_APPLICATION = 'flip_v6.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'version_6',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT':'3306',
+        'HOST': 'localhost'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
+                      "django.contrib.staticfiles.finders.AppDirectoriesFinder")
+TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader')
+
